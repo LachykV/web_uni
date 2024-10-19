5897db0becdaf0438cef65d8b3f2ff64eb54d844
LachykV <145335731+LachykV@users.noreply.github.com>
2024-10-19 13:52:46 +0000

    UPG2

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.addEventListener("partialsLoaded", () => {
+    import("./header.js");
+    import("./faq.js");
+    import("./partners.js");
+    import("./testimonials.js");
+    import("./gallery.js");
+  });
