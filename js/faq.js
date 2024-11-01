3bc67c7e83f3443b3490bd3ac4e66a0ef5d3c869
LachykV <145335731+LachykV@users.noreply.github.com>
2024-11-01 08:01:29 +0000

    New UPG

--- js/faq.js
+++ js/faq.js
@@ -2,17 +2,16 @@ const faqQuestions = document.querySelectorAll(".faq__question");
 
 faqQuestions.forEach((question) => {
   question.addEventListener("click", () => {
-    if(question.classList.contains("active")){
-      question.classList.remove("active");
-    }
-    else{
+    const isActive = question.classList.contains("active");
+
+    if (!isActive) {
       resetFaqs(faqQuestions);
-      question.classList.add("active");
     }
+
+    question.classList.toggle("active", !isActive);
   });
 });
+
 function resetFaqs(faqs) {
-  faqs.forEach((faq) => {
-      faq.classList.remove("active");
-  });
+  faqs.forEach((faq) => faq.classList.remove("active"));
 }
