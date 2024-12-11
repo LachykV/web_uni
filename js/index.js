bb222651c8f3e67cf07fb217b0baca29d24ed23b
LachykV <145335731+LachykV@users.noreply.github.com>
2024-12-11 13:41:29 +0000

    Update 11 12

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 document.addEventListener("partialsLoaded", () => {
     import("./header.js");
     import("./faq.js");
-    import("./partners.js");
-    import("./testimonials.js");
     import("./gallery.js");
   });
